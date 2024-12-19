45b9ac897868f740d56b3647f17492a595e0431f
suraj1607 <suraj1877patil@hotmail.com>
2024-12-19 09:53:55 +0530

    Duplicate Elements

---
+++ src/main/java/arrays/LargestElementInAnArray.java
@@ -0,0 +1,30 @@
+package arrays;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class LargestElementInAnArray {
+    public static void main(String[] args) {
+       int[] arr = {1, 5, 3, 6, 2, 6, 3, 7, 3};
+
+        int value = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] >  value) {
+                value = arr[i];
+            }
+        }
+
+        System.out.println(value);
+
+
+        List<Integer> collect = new ArrayList<>(IntStream.of(arr).boxed().toList());
+
+        Collections.sort(collect);
+
+        System.out.println(collect.getLast());
+
+
+    }
+}
